22a3d610c87e633f973a8b436ed7438d969bddb4
Jcdiem <jcdiem@protonmail.com>
2020-10-28 13:22:38 -0500

    terminal framework -Built the framework for the terminal -Added ENUM to handle use-cases of term window -Added a couple of example commands to the terminal

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$('#term-inputHeader').text('[/]$')
+const cliModes = {
+    TERM: 0,
+    BATTLE: 1,
+    STORY: 2,
+}
+newUser = true; //TODO: Have a session cookie to store progress, so it doesn't reset every time
+//Setup for initial users
+if(newUser){
+
+}
+
+let cliMode = cliModes.STORY; //TODO: Move cliMode to be handled by buttons/cur screen
+if(!cliMode){
+    throw new Error('cliModes ENUM is borked')
+}
+
+
+
+
+$('#term-inputBox').keydown(function(e){
+    if(e.key === 'Enter'){
+        switch (cliMode){
+            case cliModes.TERM:
+                termCommand($('#term-inputBox').val());
+                break;
+            case cliModes.STORY:
+                break;
+            case cliModes.BATTLE:
+                break;
+            default:
+                console.error('value set out of bounds of enum')
+                break;
+        }
+        $('#term-inputBox').val('');
+    }
+});
+
+
---
+++ js/term.js
@@ -0,0 +1,75 @@
+function termCommand(input){
+    termOuput($('#term-input').text() + input);
+    const args = String(input).trim().split(/ +/g)
+    const cmd = args.shift().toLowerCase();
+
+    switch(cmd){
+        case 'help':
+            termOuput('Availble commands are \'cd\', \'clear\', and \'help\'')
+            break;
+
+        case 'cd':
+            if(args[0] === '..'){
+                changePath('desc')
+            }
+            else{
+                changePath('asc', args[0])
+            }
+            break;
+
+        case 'clear':
+            $('#term-output').empty();
+            //Resend last, so they know they just cleared
+            termOuput($('#term-input').text() + input);
+            break;
+
+        default:
+            termOuput('Unknown command \'' + cmd + '\', try \'help\'');
+            break;
+    }
+}
+
+function termOuput(txt){
+    $('#term-output').append('<p class="termItem">' + txt + '</p>');
+}
+
+function changePath(type, text=''){
+    const curPath = String($('#term-inputHeader').text());
+
+    //Move to new folder
+    if(type === 'asc'){
+        //If statement to prevent prepending a '/' when the current dir is just '[/]$'
+        if(curPath.charAt(2) === ']'){
+            $('#term-inputHeader').text(curPath.substr(0,curPath.length-2) + text + ']$')
+        }
+        else{
+            $('#term-inputHeader').text(curPath.substr(0,curPath.length-2) + '/' + text + ']$')
+        }
+    }
+    //Go down a folder
+    else if(type === 'desc'){
+        let tmpPath = curPath.split('/');
+        tmpPath[tmpPath.length - 1] = '';
+
+        //Make the new path from the array of path parts
+        let pathOut = '';
+        //TODO: Find out why tmpPath has a leading ' ' that is counted
+        for(let i = 0; i < tmpPath.length - 1; i++){
+            //Add dest to path
+            pathOut += tmpPath[i];
+
+            //Add the root slash
+            if(i === 0){
+                pathOut = pathOut.substr(1);
+                pathOut = '[/'+pathOut;
+            }
+            //Prevent adding a slash at the final folder
+            else if(!( (i + 2) === tmpPath.length)){
+                pathOut += '/';
+            }
+        }
+        pathOut += ']$';
+
+        $('#term-inputHeader').text(pathOut);
+    }
+}
